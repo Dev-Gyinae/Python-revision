88d779074c1fb58b67478145069a8b3e1a9394cf
Samuel Gyinae <dev.gyinae@gmail.com>
2026-03-18 20:05:11 +0000

    prs

--- rpc.py
+++ rpc.py
@@ -1,32 +1,52 @@
 import random
 
-# Player input
-player_choice = input('.. enter \n 1 for rock \n 2 for paper \n 3 for scissor \n .....')
-choice = int(player_choice)
+def play_rps():
+    player_score = 0
+    computer_score = 0
+    ties = 0
 
-# Validation
-if choice < 1 or choice > 3:
-    print('start again')
-else:
-    # map number to word
-    names = {'1': 'rock', '2': 'paper', '3': 'scissor'}
-    print(f'player chose {names[player_choice]}')
+    Game_Active = True
 
-# Computer choice
-computer_choice = random.choice(['1', '2', '3'])
-print(int(computer_choice))
-print(f'computer chose {names[computer_choice]}')
+    while Game_Active:
+        Play_again = input("\n\n press 'y' to play or 'q' to quit: \n")
+        if Play_again.lower() == 'y':
+            player_input = input('choose: \n 1 for rock \n 2 for paper \n 3 for scissor \n\n')
 
-# Winner logic
-if player_choice == computer_choice:
-    print('its a draw')
-elif player_choice == '1' and computer_choice == '3':
-    print('player won')
-elif player_choice == '2' and computer_choice == '1':
-    print('player won')
-elif player_choice == '3' and computer_choice == '2':
-    print('player won')
-else:
-    print('computer won')
+            # validation loop
+            while player_input not in ['1', '2', '3']:
+                print("invalid input! Try again!")
+                player_input = input('choose: \n 1 for rock \n 2 for paper \n 3 for scissor \n\n')
 
-'''rock beats scissor, scissor beats paper, paper beats rock'''
+            player_choice = int(player_input)
+            computer_choice = int(random.choice('123'))
+
+            names = {1:'rock', 2:'paper', 3:'scissor'}
+
+            print(f'You chose: {names[player_choice]} and computer chose: {names[computer_choice]}')
+
+            if player_choice == 1 and computer_choice == 3:
+                print('player wins')
+                player_score += 1
+            elif player_choice == 2 and computer_choice == 1:
+                print('player wins')
+                player_score += 1
+            elif player_choice == 3 and computer_choice == 2:
+                print('player wins')
+                player_score += 1
+            elif player_choice == computer_choice:
+                print("it's a tie")
+                ties += 1
+            else:
+                print('computer wins')
+                computer_score += 1
+
+            print(f"Score -> Player: {player_score}, Computer: {computer_score}, Ties: {ties}")
+
+        elif Play_again.lower() == 'q':
+            print('\n Thank you for playing')
+            print(f"Final Score -> Player: {player_score}, Computer: {computer_score}, Ties: {ties}")
+            Game_Active = False
+        else:
+            print("Invalid selection, please press 'y' or 'q'.")
+
+play_rps()
