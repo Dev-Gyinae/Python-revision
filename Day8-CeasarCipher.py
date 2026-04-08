5432920c310a7bc1dcae3ff55e3b7c11ee73289b
Samuel Gyinae <dev.gyinae@gmail.com>
2026-04-08 18:39:00 +0000

    Fixed with Input Validation

--- Day8-CeasarCipher.py
+++ Day8-CeasarCipher.py
@@ -1,5 +1,4 @@
 # Caesar's Cipher
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -15,16 +14,37 @@ def caesar(start_text, shift_amount, cipher_direction):
             end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
 
+def get_valid_direction():
+    while True:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+        if direction in ["encode", "decode"]:
+            return direction
+        print("Invalid input! Please type 'encode' or 'decode'.")
+
+def get_valid_shift():
+    while True:
+        try:
+            shift = int(input("Type the shift number (1-25):\n"))
+            if 1 <= shift <= 25:
+                return shift
+            print("Please enter a number between 1 and 25.")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+
 should_continue = True
 
 while should_continue:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    direction = get_valid_direction()
     text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    shift = get_valid_shift()
     
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
     
-    result = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
-    if result == "no":
-        should_continue = False
-        print("Goodbye!")
+    while True:
+        result = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+        if result in ["yes", "no"]:
+            if result == "no":
+                should_continue = False
+                print("Goodbye!")
+            break
+        print("Invalid input! Please type 'yes' or 'no'.")
