1ef558236f756beebaafc435308584561091de28
Samuel Gyinae <dev.gyinae@gmail.com>
2026-04-08 18:36:29 +0000

    while logic

--- Day8-CeasarCipher.py
+++ Day8-CeasarCipher.py
@@ -1,11 +1,8 @@
-#Cesars Cipher
+# Caesar's Cipher
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def ceasar(start_text, shift_amount, cipher_direction):
+def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
@@ -18,4 +15,16 @@ def ceasar(start_text, shift_amount, cipher_direction):
             end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
 
-ceasar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+    if result == "no":
+        should_continue = False
+        print("Goodbye!")
