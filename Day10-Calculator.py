6eed432205b38e5ec97d8b819b6289dffd0c577e
Samuel Gyinae <dev.gyinae@gmail.com>
2026-04-09 19:00:44 +0000

    calc full

--- Day10-Calculator.py
+++ Day10-Calculator.py
@@ -1,36 +1,40 @@
 #Calculator
-
 def add(x, y):
-    return x + y    
+    """This function adds two numbers."""
+    return x + y
+
 def subtract(x, y):
-    return x - y    
+    """This function subtracts two numbers."""
+    return x - y
+
 def multiply(x, y):
-    return x * y
+    """This function multiplies two numbers."""
+    return x * y     
+
 def divide(x, y):
+    """This function divides two numbers."""
+    if y == 0:
+        return "Error: Division by zero is not allowed."
     return x / y
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
-    else:
-        print("Invalid Input")
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+x = float(input("Enter first number: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the list above: ")
+y = float(input("Enter second number: "))
+
+calculation_function = operations[operation_symbol]
+result = calculation_function(x, y)
+print(f"{x} {operation_symbol} {y} = {result}")
 
 
 # Docstring
